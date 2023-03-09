1ca3419453b30f13a0abc5b0317cb7b047dafb40
josman2 <100327586+josman2@users.noreply.github.com>
2023-03-08 20:31:24 -0500

    Add files via upload

---
+++ arreglos/proceso.php
@@ -0,0 +1,53 @@
+<?php
+/*
+$comidas[0] = "mondongo";
+$comidas[1] = "hamburguesa";
+$comidas[2] = "picada";
+$comidas[3] = "arroz con pollo";
+
+
+$planta = [" rosa ", " girasol ", "margarita" ];
+$color = [" rojo ", " amarillo ", " blancas " ];
+
+$array = array_combine($planta,$color);
+//print_r(array_keys($array));
+//print_r(array_values($array));
+print_r(array_flip($array));
+
+
+
+$Color1 = 'Amarillo';
+$Color2 = 'Azul';
+$Color3 = 'Rojo';
+
+$array = compact('Color1', 'Color2', 'Color3');
+print_r($array);
+*/
+
+
+
+/*$coches = [10,23,35,17,12];
+echo $coches[3];*/
+
+/*
+$array = [
+ "coches1" => 10,
+ "coches2" => 23,
+ "coches3" => 35,
+ "coches4" => 17,
+ "coches5" => 12,
+];*/
+
+//extract($array);
+
+//echo("$coches1,$coches2,$coches3,$coches4,$coches5");
+
+/*$coches1 = 10;
+$coches2 = 23;
+$coches3 = 35;
+$coches4 = 17;
+$coches5 = 12;
+
+$array = compact("coches1","coches2","coches3","coches4","coches5");
+print_r($array)*/
+?>
